d9a10ae30cff0cb21d9fb91938f540887f588dec
ElDav1d <dvivoes@yahoo.es>
2023-02-11 17:38:00 +0100

    Refactor to async functions

--- routes/index.js
+++ routes/index.js
@@ -9,40 +9,38 @@ router.get("/", (req, res, next) => {
 
 module.exports = router;
 
-router.get("/movies", (req, res, next) => {
-  MovieModel.find()
-    .select({ title: 1, image: 1 })
-    .then((response) => {
-      res.render("movies.hbs", {
-        movieList: response,
-      });
-    })
-    .catch((error) => {
-      next(error);
+router.get("/movies", async (req, res, next) => {
+  try {
+    const movieList = await MovieModel.find().select({ title: 1, image: 1 });
+
+    res.render("movies.hbs", {
+      movieList,
     });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // GET movie/:id => render one movie details (image, title, director, stars, description, showtime) by its id
 
-router.get("/movie/:id", (req, res, next) => {
+router.get("/movie/:id", async (req, res, next) => {
   const { id } = req.params;
-  MovieModel.findById(id)
-    .then((response) => {
-      const { image, title, director, stars, description, showtimes } =
-        response;
 
-      res.render("movie.hbs", {
-        movieDetails: {
-          image,
-          title,
-          director,
-          stars,
-          description,
-          showtimes: showtimes.join(" | "),
-        },
-      });
-    })
-    .catch((error) => {
-      next(error);
+  try {
+    const { image, title, director, stars, description, showtimes } =
+      await MovieModel.findById(id);
+
+    res.render("movie.hbs", {
+      movieDetails: {
+        image,
+        title,
+        director,
+        stars,
+        description,
+        showtimes: showtimes.join(" | "),
+      },
     });
+  } catch (error) {
+    next(error);
+  }
 });
